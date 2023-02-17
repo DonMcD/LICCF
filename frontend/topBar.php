e67e4541ad410f1a4779ca914adc6a93331a5358
Donavon McDowell <90471623+DonMcD@users.noreply.github.com>
2023-02-17 10:47:35 -0700

    Updated Frontend/ Backend

--- frontend/topBar.php
+++ frontend/topBar.php
@@ -1,10 +1,10 @@
 <div class="top-bar">
-    <a href='../backend/createNewPerson.php'><div>Manage People</div></a>
-    <a href='../backend/createNewFamily.php'><div>Manage Families</div></a>
+    <a href='../frontend/createNewPerson.php'><div>Manage People</div></a>
+    <a href='../frontend/createNewFamily.php'><div>Manage Families</div></a>
     <a href=''><div>Manage Donations</div></a>
     <a href=''><div>Issue Tax Reciepts</div></a>
-    <a href='../backend/sendMassEmail.php'><div>Send Mass Email</div></a>
+    <a href='../frontend/sendMassEmail.php'><div>Send Mass Email</div></a>
     <a href=''><div>View Attendance</div></a>
-    <a href='../backend/modifyZoomLink.php'><div>Modify Zoom Link</div></a>
-    <a href='../backend/modifyCalendar.php'><div>Modify Calendar</div></a>
+    <a href='../frontend/modifyZoomLink.php'><div>Modify Zoom Link</div></a>
+    <a href='../frontend/modifyCalendar.php'><div>Modify Calendar</div></a>
 </div>
