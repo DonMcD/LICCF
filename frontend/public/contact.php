e21525aedbfae21689f8c9daefb79712809ea1e6
Donavon McDowell <90471623+DonMcD@users.noreply.github.com>
2023-02-19 16:15:42 -0700

    Updated Pages
    Added header to all basic pages

---
+++ frontend/public/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name= "author" content= "VARchitects">
+    <title>LICCF Login</title>
+    <link rel="stylesheet" href="../../css/styles.css">
+    <link rel="stylesheet" href="../../css/dashboard.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+</head>
+<body class="backgrounds">
+<?php
+require '../headers/header.php';
+?>
+
+
+
+</body>
+</html>
