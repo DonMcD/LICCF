c5f3b568a5cd137f1b0a1a084b2ccdc53c7ec762
Donavon McDowell <90471623+DonMcD@users.noreply.github.com>
2023-02-19 17:45:28 -0700

    Updated Header with Dropdowns

--- frontend/headers/header.php
+++ frontend/headers/header.php
@@ -1,19 +1,46 @@
 <header>
-    <h2>LICCF</h2>
-    <a href="../../frontend/public/index.php">Home</a>
-    <a href="../../frontend/public/eucharist.php">Eucharist</a>
-    <a href="../../frontend/public/community.php">Community</a>
-    <a href="../../frontend/public/contact.php">Contact</a>
-    <a href="../../frontend/public/donate.php">Donate</a>
-    <?php
-    displayAuthLink();
-    ?>
+  <h2>LICCF</h2>
+  <nav>
+    <ul>
+      <li><a href="../../frontend/public/index.php">Home</a></li>
+      <li class="dropdown">
+        <a href="../../frontend/public/eucharist.php">About</a>
+        <ul class="dropdown-content">
+          <li><a href="#">Who we are</a></li>
+          <li><a href="#">Vision Statement</a></li>
+          <li><a href="#">Our History</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="../../frontend/public/eucharist.php">Eucharist</a>
+        <ul class="dropdown-content">
+          <li><a href="#">Liturgies</a></li>
+          <li><a href="#">Sacraments</a></li>
+          <li><a href="#">Special Services and Blessings</a></li>
+          <li><a href="#">Prayer</a></li>
+          <li><a href="#">Spiritual Direction</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="../../frontend/public/community.php">Community</a>
+        <ul class="dropdown-content">
+          <li><a href="#">News and Events</a></li>
+          <li><a href="#">Get Involved</a></li>
+          <li><a href="#">Indigenous Allyship</a></li>
+        </ul>
+      </li>
+      <li><a href="../../frontend/public/contact.php">Contact</a></li>
+      <li><a href="../../frontend/public/donate.php">Donate</a></li>
+      <?php displayAuthenticatedLink(); ?>
+    </ul>
+  </nav>
 </header>
 
 <?php
-function displayAuthLink() {
+//This function determines if the button should say login or logout based on the Authenticated SESSION variable
+function displayAuthticatedLink() {
     if (isset($_SESSION['authenticated']) && $_SESSION['authenticated']) {
-      echo '<a href="../../backend/logout.php">Logout</a>';
+      echo "<a href='../../backend/logout.php' id='login-btn'>Logout</a>";
     } else {
       echo '<a href="../../frontend/public/login.php">Login</a>';
     }
