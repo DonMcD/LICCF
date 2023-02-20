2cdfb3dfae3375d28613b0fb1aacb65699f80b72
AnasAfri <90427484+AnasAfri@users.noreply.github.com>
2023-02-20 13:30:06 -0700

    Update community.php
    added image and text in the community page

--- frontend/public/community.php
+++ frontend/public/community.php
@@ -9,12 +9,45 @@
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+    <style>
+      #container {
+        position: relative;
+        width: 1920px;
+        height: 456px;
+      }
+      
+      #image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+      
+      #text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 48px;
+        text-align: center;
+        color: white;
+        text-shadow: 2px 2px 4px black;
+      }
+    </style>
 </head>
 <body class="backgrounds">
 <?php
 require '../headers/header.php';
 ?>
-
+<!--This code create the Image and the text in the page-->
+    <div id="container">
+        <img src="" alt="">
+        <div id="text">
+            Welcome to LICCF <br>
+            Lethbridge Inclusive Catholic Community Felloship
+        </div>
+    </div>
 
 
 </body>
