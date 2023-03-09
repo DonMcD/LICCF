886dcf647f80aca89bcd8b2a13b0771e5ed90d6f
Renalyn0913 <120226467+Renalyn0913@users.noreply.github.com>
2023-03-08 22:26:43 -0700

    prayer

--- frontend/public/prayer.php
+++ frontend/public/prayer.php
@@ -37,39 +37,174 @@ require '../headers/header.php';
 			max-width: 500px;
 			margin-bottom: 20px;
 		}
+
+		h1 {
+			color: #3E702D;
+			text-align: center;
+		}
+		
 	</style>
 </head>
 <body>
-	<h1 style="text-align:center">Stillness Practice with Vickie MacArthur</h1>
+	<h1 style="text-align:center">Prayers with Words, Silence, Body and Nature</h1>
 	<div class="textbox">
-		<a href="https://www.vickiemacarthur.ca/spiritual-direction/" style="text-align: center;">Click here to access the Stillness Practice</a>
+		<p style="text-indent: 50px;">
+		<a href="https://crossministrygroup.org/wp-content/uploads/2019/06/R-Group-Process-Welcoming-Prayer.pdf" 
+		style="text-align: center;">The Welcoming Prayer</a> from Contemplative Outreach is a prayer of meeting with respect, 
+			in both body and mind, whatever emotion rises in you.</p>
 	</div>
 	
-<h1 style="text-align:center">Lectio Divina</h1>
+<h1 style="text-align:center">Lectio Divina, sacred reading</h1>
 <div class="textbox">
-	<p>Instructions for praying Lectio Divina:</p>
+	<p style="text-indent: 50px;">If you are reading a prayer or scripture at a liturgy, this practice is good preparation.</p>
+	<p style="text-indent: 50px;">Begin sitting, and take a big breath in, acknowleging the Spirit’s presence and exhaling your 
+		worries or preoccupying thoughts into the Presence of the Divine all around you, Let go of the busyness of the day or of the mind. 
+		Prayerfully chose a line or passage of scripture, perhaps from a psalm or the gospels. 
+		A line from a poem or a complete poem can also be prayed in this practice 
+		(Poet/Author suggestions: Mary Oliver, Luci Shaw, Richard Wagamese, Joan Chittester, John O’Donohue, 
+		Antoinette Voûte Roeder, Rumi).  Center yourself once more. 
+		Note your feet on the floor, the rise and fall of your chest or belly with natural breathing. Then:
 	<ul>
-		<li>Choose a passage from Scripture</li>
-		<li>Read the passage slowly and meditatively</li>
-		<li>Reflect on the passage and what it means to you</li>
-		<li>Respond to the passage in prayer</li>
-		<li>Rest in God's presence</li>
+		<li>Read aloud the passage noting any words or phrases that catch your attention or curiosity or have emotional resonance with you. 
+			After reading, silently repeat this word or phrase many times by itself, considering the images or significance that arise. 
+			What do I notice? Quiet yourself and then:</li>
+		<li>Read aloud the pass a second time and make sense of it in terms of characters, action, intentions of the text itself. 
+			If you have a commentary on the scripture, inform yourself of the history and cultural significance of this text. 
+			Sit quietly and ponder. What is this passage saying?</li>
+		<li>A third time read the passage aloud. Sit quietly for at least 2 minutes and ask prayerfully, 
+			“How does my life fit with this passage? What do you want me to see?”</li>
+		<li>In the fourth reading, relax and receive the words like an embrace. 
+			What prayer arises in me? Or What action is prompted in me?</li>
+		<li>Share your experience as a journal entry, or with a loved one or spiritual director.</li>
 	</ul>
 </div>
 
-<h1 style="text-align:center">CinemaDivina</h1>
+<h1 style="text-align:center">Visio Divina, sacred seeing</h1>
 <div class="textbox">
-	<a href="https://vimeo.com/channels/cinemadivina" style="text-align: center;">Click here to access CinemaDivina</a>
+	<p style="text-indent: 50px;">after Christine Valters Paintner in <i> Lectio Divina </i> (2008) p. 212-124 </p>
+	<p style="text-indent: 50px;">Chose an icon, photograph, scene or image and before examining it at all, 
+		read these instructions over.</p>
+	<ul>
+		<li>Sit in a quiet spot with the scene before you or image self-supported in front of you.</li>
+		<li><b>GAZE</b> Close your eyes and invite the Holy Spirit’s guidance. Breathe in and out a few times and relax.</li>
+		<li><b>REFLECT</b> When ready to begin, open your eyes to the image and stay with the first thing you see. 
+			For several moments, notice the shapes, contours, colour of that tiny area on which your eye first alighted. 
+			Internalize it. Receive it with gratitude. What prayer arises in me? Or What action is prompted in me?</li>
+		<li><b>RESPOND</b> Now move your eye a little further to see how this small piece fits into the larger image. 
+			What function does it have? Is it something significant of a larger aspect? Honour what you have discovered by 
+			spending time considering how this small piece is related to the larger image and intention of the icon/statue/painting etc. 
+			What is the invitation to you that is expressed in this aspect of the image? How do you respond? 
+			Take some time to ponder in silence and perhaps journal.</li>
+		<li><b>REST</b> in God. After you have allowed this image to speak to your heart, release the words and colours 
+			and shapes to God and close your eyes. Open to God’s blessing in this darkness and give thanks. </li>
+	<p><b>CLOSING</b> You may now just want to look at the icon freely and become “friends” with it. You may want to explore it through drawing or photograph it. Honour the prayer within you that drew you closer to God’s love.</p>	
+	</ul>
 </div>
 
-<h1 style="text-align:center">Listen for Joy</h1>
+<h1 style="text-align:center">The Jesus Prayer</h1>
 <div class="textbox">
-	<a href="https://listenforjoy.com/" style="text-align: center;">Click here to access Listen for Joy</a>
+	<p style="text-indent: 50px;">(also recited as “The Lord’s Prayer” or “Our Father”)</p>
+	<p style="text-indent: 50px;">The Jewish Aramaic words for the translations of “Lord” and “Father” in heaven, are Abwoon D’Bwashmaya and are not masculine. 
+		Their strongest connotation is “birther.” LICCF advocates for inclusive language and a non- or multi-gendered connection with the Divine. 
+		You can find out more about the Aramaic phrase and its cultural significance 
+	<a href="https://www.rollingridge.org/post/abwoon-dbwashmaya-12838927" style="text-align: center;">here</a> </p>
+	<p style="text-indent: 50px;">You can hear the prayer spoken in Jesus’ mother tongue 
+	<a href="https://www.youtube.com/watch?v=i8IJOgMVE1Q" style="text-align: center;">here</a> </p>
+	<p style="text-indent: 50px;">A contemporary reading of this prayer in scripture can be found in the <i> First Nations Version of the New 
+		Testament </i> Luke or <i> Shining Light </i> 11:2-4 or Matthew or <i> Gift From Creator </i> 6:9-13. </p>
+	<p style="text-indent: 50px;">Finally, here is a revisioning of the prayer by Juleta Severson-Baker of Calgary in her Spirit-filled 
+		book of poetry <i> Every Word Spoken </i> (Leaf Press, 2013) p. 30.</p>
+	<p style="text-align:center"> <b> THE LORD’S PRAYER: A REVISIONING </b> </p>
+	<p style="text-align:center"> In awe we come to You again, <br> You, whose name remains beyond all naming. <br>
+		May Your will be our will, <br> Your peace be peace on earth, <br> And what is broken in our lives <br> 
+		be forgiven and made right. <br> Grant us today the bread of Your fields. <br> From the labour of our days <br>
+		may Your love be the yield. <br> For You are, have been, and <br> always will be, our source, <br> our end, our souls’ delight. </p>
 </div>
 
-<h1 style="text-align:center">BraveJoy sharing</h1>
+<h1 style="text-align:center">The Rosary</h1>
 <div class="textbox">
-	<a href="https://bravejoy.net/" style="text-align: center;">Click here to access BraveJoy sharing</a>
+	<p style="text-indent: 50px;"> This is a traditional Catholic devotion prayed by many non-Catholics, too. Abbey of the Arts
+	<a href="https://abbeyofthearts.com/" style="text-align: center;">abbeyofthearts.com</a>
+		provides modern reflections of prayer honour Mary the mother of Jesus. You will find many YouTube possibilities of praying this 
+		string of prayers aloud with various scriptures on certain days of the week. <br> We suggest the following inclusive language changes 
+		that you may want to incorporate in your recitation of this prayer </p>
+	<p style="text-indent: 50px;"> The SIGN OF THE CROSS <br>
+		In the name of God/de: Our Creator, Jesus our Friend and the Holy Spirit.</p>
+	<p style="text-indent: 50px;"> The CREED <br>
+		The Apostle’s Creed (from <i> credo </i> “I believe” is prayed at the outset of the Rosary. 
+		Here are two contemporary, inclusive, theologically open renditions of the creed. </p> <br>
+	<p style="text-align:center" > <b> A New Creed </b> (1968) </p>
+	<p style="text-align:center">	We are not alone, <br> we live in God’s world. <br> We believe in God: <br>
+    	who has created and is creating,<br> who has come in Jesus, <br> the Word made flesh, <br>
+       	to reconcile and make new, <br> who works in us and others <br> by the Spirit. <br>
+		We trust in God. <br> We are called to be the Church: <br> to celebrate God’s presence, <br>
+    	to live with respect in Creation, <br> to love and serve others, <br> to seek justice and resist evil, <br>
+    	to proclaim Jesus, crucified and risen,<br> our judge and our hope. <br> In life, in death, in life beyond death, <br>
+    	God is with us. <br> We are not alone. <br> Thanks be to God. <br> <br> (United Church of Canada) </p> <br>
+	<p style="text-align:center"> <b> Creed </b> </p>
+	<p style="text-align:center"> We believe in the Holy One, <br> who is love poured out in creation. <br>
+		We believe in Jesus, who showed us how to live, and <br> who prayed that “All may be one.” <br>
+		We believe that we are called to follow the Spirit, <br> the source of wisdom and truth <br>
+		within, around and among us. <br> We believe that we are the Christ Presence <br>
+		evolving here and now as we live justly, love tenderly <br> and walk with integrity. <br>
+		Amen. <br> <br> (ARCWP, USA) </p> <br>
+	<p> <b> THE LORD’S PRAYER </b> – see above “The Jesus Prayer” </p> <br>
+	<p style="text-align:center"> <b> HAIL MARY </b> </p>
+	<p style="text-indent: 50px;"> Pádraig Ó Tuama, Irish theologian and poet, suggests the word “Hello” for “Hail” which, in Luke’s gospel, 
+		is Elizabeth’s greeting to Mary while both women are pregnant. </p>
+	<p style="text-align:center"> “Hello Mary. Our God/de is with you. <br>
+		Blessed are you among women. <br>
+		And Blessed is the fruit of your womb, Jesus. <br>
+		Holy Mary, Mother of God <br>
+		Pray for us pilgrims now and at the hour of our death.” <br> Amen. </p>
+</div>
+
+<h1 style="text-align:center">A Lenten Prayer Practice</h1>
+<div class="textbox">
+	<p style="text-indent: 50px;"> Diane Millis PhD., professor, spiritual director, and retreat facilitator 
+		suggests a Lenten practice of taking and pondering one picture a day in a March 2023 practice 
+		called “Look Closely. Listen Deeply. Linger Intentionally.” Find it
+	<a href="https://mail.google.com/mail/u/0/#search/diane.millis%40gmail.com/FMfcgzGrcrvghMSPkQgLwVpQVQBFVVKp" 
+		style="text-align: center;">here</a> </p>
+</div>
+
+<h1 style="text-align:center">Other Resources</h1>
+<div class="textbox">
+	<p style="text-indent: 50px;"> <i> Body practice</i>, a gentle use of chants with exercises after Resmaa Menakem MSW, 
+		LICSW, SEP, author of <i> My Grandmother’s Hands </i>, is available by Zoom with Teresa Hanlon every Tuesday and 
+		Saturday mornings at 9 AM Mountain Time for 15 minutes in duration. This is the
+	<a href="https://us02web.zoom.us/j/81456029918" style="text-align: center;">LINK</a> to join.</p>
+</div>
+
+<h1 style="text-align:center">Song & Silence</h1>
+<div class="textbox">
+	<p style="text-indent: 50px;"> Fourth Sundays of the month at 4 pm at Hanlons during Lent and perhaps through 
+		the year if there is interest, we celebrate Song & Silence in the style of Taizé. Inquire at
+	<a href="mailto:LICComm2022@gmail.com" 
+		style="text-align: center;">LICComm2022@gmail.com</a> for more information. </p>
+</div>
+
+<h1 style="text-align:center">Healing Prayer</h1>
+<div class="textbox">
+	<p style="text-indent: 50px;"> Teresa is available for healing prayer
+	<a href="mailto:teresa.hanlon@gmail.com" 
+		style="text-align: center;">teresa.hanlon@gmail.com</a> and can recommend the Ignatian “Examen” as 
+			presented in the book <i> Sleeping with Bread: Holding What Gives You Life,</i>  by Dennis Linn, 
+			Sheila Fabricant Linn and Matthew Linn. </p>
+</div>
+
+<h1 style="text-align:center"> Eco Prayer</h1>
+<div class="textbox">
+	<p style="text-indent: 50px;"> At Thanksgiving a group of us gathers to go into the coulee to pray thanks 
+	with the Tobacco Ceremony from Richard <i> Wagamese’s One Drum. </i> </p>
+	<p style="text-indent: 50px;"> We can listen to the messages of the natural world. 
+	Here are some sites to learn more. An ecochurch from the UK: </p>
+	<p style="text-indent: 50px;"> <a href="https://ecochurch.arocha.org.uk/wp-content/uploads/2018/05/Green-Communion-Prayers.pdf" 
+		style="text-align: center;">Green Communion Prayers</a> </p>
+	<p style="text-indent: 50px;"> <a href="https://www.jesuits.org/wp-content/uploads/2021/04/Boreal-Forest-Prayer.pdf" 
+		style="text-align: center;">Jesuit Boreal Forest Prayer</a> </p>
+	<p style="text-indent: 50px;"> <a href="https://www.morethantokyo.com/forest-bathing-shinrin-yoku/" 
+		style="text-align: center;">Forest Bathing</a> </p>
 </div>
 
 <?php include_once ('..\..\frontend\footers\footer.php'); ?>
